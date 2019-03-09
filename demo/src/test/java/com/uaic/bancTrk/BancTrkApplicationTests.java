3c15aa4a4c629ff6d5fa1bfbb956a2310e025512
Anamaria Goldan <ana.goldan@yahoo.com>
2019-03-09 14:30:11 +0200

    Am adaugat un demo in folderul de root

---
+++ demo/src/test/java/com/uaic/bancTrk/BancTrkApplicationTests.java
@@ -0,0 +1,16 @@
+package com.uaic.bancTrk;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BancTrkApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
